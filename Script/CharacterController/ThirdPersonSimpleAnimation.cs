50bf9933f3f1b7be491df3d07effc77040c00791
Maria Osipova <maria.osipova@mail.huji.ac.il>
2014-11-08 18:08:42 +0200

    Update ThirdPersonSimpleAnimation.cs

--- Script/CharacterController/ThirdPersonSimpleAnimation.cs
+++ Script/CharacterController/ThirdPersonSimpleAnimation.cs
@@ -1,14 +1,8 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="ThirdPersonSimpleAnimation.cs" company="Scalify">
-//     Copyright (c) 2012 Scalify. All rights reserved.
-// </copyright>
-//-----------------------------------------------------------------------
-  
 using UnityEngine;
 
-/// <summary>
-/// A simple third person controller which is basically it copied from ThirdPersonController.js scrip
-/// </summary>
+//
+// A simple third person controller 
+// </summary>
 public class ThirdPersonSimpleAnimation : MonoBehaviour
 {
     public ThirdPersonController controller;
@@ -39,8 +33,6 @@ public class ThirdPersonSimpleAnimation : MonoBehaviour
     {
         if (controller == null)
         {
-            // sync replica animation.
-            this.animation["Run"].normalizedSpeed = 1.0f;
             this.animation["Walk"].normalizedSpeed = 1.0f;
 
             this.animation.CrossFade(this.animationName);
@@ -50,32 +42,19 @@ public class ThirdPersonSimpleAnimation : MonoBehaviour
 
             float currentSpeed = controller.GetSpeed();
 
-            if (currentSpeed > controller.walkSpeed)
-            {
-                animation.CrossFade("Run");
-                animationName = "Run";
-            }
-            else if (currentSpeed > 0.1)
+			if (currentSpeed > 0.1)
             {
                 animation.CrossFade("Walk");
                 animationName = "Walk";
             }
             else
             {
+
                 animation.CrossFade("Idle");
                 animationName = "Idle";
             }
-
-            animation["Run"].normalizedSpeed = runSpeedScale;
             animation["Walk"].normalizedSpeed = walkSpeedScale;
-
-            if (controller.IsJumping())
-            {
-				animation.CrossFade("Run");
-				animationName = "Run";
-            }
         }
-		this.animation.Play(this.animationName);
     }
     #endregion
 }
